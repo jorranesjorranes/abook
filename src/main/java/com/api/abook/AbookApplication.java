ac8bd203851113f36ce6d04db1970d621aaf5c70
Jorranes <jorranes.oliveira.a@gmail.com>
2023-06-30 17:22:14 -0300

    Projeto inicializado

---
+++ src/main/java/com/api/abook/AbookApplication.java
@@ -0,0 +1,13 @@
+package com.api.abook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/abook/AbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.abook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
